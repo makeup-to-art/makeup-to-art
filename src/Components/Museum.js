44d60dc86247be4ede2ce7a29c1817eb1019a0d4
Ryan May <ryan777may@gmail.com>
2021-10-13 19:30:56 -0400

    Updated comments

--- src/Components/Museum.js
+++ src/Components/Museum.js
@@ -8,54 +8,48 @@ const Museum = (props) => {
     const updateArtwork = props.updateArtwork;
     const [error,
         setError] = useState()
-    const color = " " + props.colorChoice; //add a space cause api is retarded af
+    const color = " " + props.colorChoice; //add a space cause api because API quirks
     const [artworkPresent,
         setArtworkPresent] = useState(false);
 
-
     useEffect(() => {
-      const randomPainting = (artArray) => {
-        const paintings = artArray.artObjects
-        const randomize = () => paintings[Math.floor(Math.random() * paintings.length)];
-        let newPainting = randomize()
-        let repeat = false;
+        const randomPainting = (artArray) => {
+            const paintings = artArray.artObjects
+            const randomize = () => paintings[Math.floor(Math.random() * paintings.length)];
+            let newPainting = randomize()
+            let repeat = false;
 
-    if (artwork.length === 0) { //Skip repeat check if gallery is just being started
-          const galleryArr = artwork;
-          galleryArr.push(newPainting)
-          updateArtwork(galleryArr)
-          setArtworkPresent(true)
+            if (artwork.length === 0) { //Skip repeat check if gallery is just being started
+                const galleryArr = artwork;
+                galleryArr.push(newPainting)
+                updateArtwork(galleryArr)
+                setArtworkPresent(true)
 
-    } else {
+            } else {
 
-      let x = 0;
-      while (x < artwork.length) {
-        if(artwork[x].id === newPainting.id){ //Check if existing paintings id's match new painting
-          repeat = true;
-          break
+                let x = 0;
+                while (x < artwork.length) {
+                    if (artwork[x].id === newPainting.id) { //Check if existing paintings id's match new painting
+                        repeat = true;
+                        break
+                    }
+                    x++
+                }
+
+                if (repeat === false) { //If there are no repeats, push the painting to the gallery
+                    const galleryArr = artwork;
+                    galleryArr.push(newPainting)
+                    updateArtwork(galleryArr)
+                    setArtworkPresent(true)
+                } else if (artwork.length !== artArray.artObjects.length) { //If we havent reached the end of the results and there is a repeat, run this function again for a new painting
+                    randomPainting(artArray)
+                } else { //
+                    setArtworkPresent(true) //Show gallery and notify user we have reached the end of the results
+                    setError('All results have been shown')
+                }
+            }
+
         }
-        x++
-      }
-
-
-      if (repeat === false) { //If there are no repeats, push the painting to the gallery
-        const galleryArr = artwork;
-        galleryArr.push(newPainting)
-        updateArtwork(galleryArr)
-        setArtworkPresent(true)
-      }
-
- 
-      else if (artwork.length !== artArray.artObjects.length){ //If we havent reached the end of the results and there is a repeat, run this function again for a new painting
-        randomPainting(artArray)
-      }else { //
-        setArtworkPresent(true) //Show gallery and notify user we have reached the end of the results
-        setError('All results have been shown')
-      }
-    }
-
-
-    }
 
         axios({
             url: 'https://www.rijksmuseum.nl/api/en/collection',
@@ -78,7 +72,7 @@ const Museum = (props) => {
     }, [color, artwork, updateArtwork])
 
     return (
-      <div className='artContainer'>
+        <div className='artContainer'>
             {artworkPresent
                 ? <div className="art">
                         {artwork.map((art, index) => {
@@ -89,8 +83,12 @@ const Museum = (props) => {
                                 </div>
                             )
                         })
-                        }
-                        <button className='clearButton' onClick={() => {props.clearGallery()}}>Clear Gallery</button>
+}
+                        <button
+                            className='clearButton'
+                            onClick={() => {
+                            props.clearGallery()
+                        }}>Clear Gallery</button>
                     </div>
                 : <p>Getting some artwork, please wait</p>
 }
