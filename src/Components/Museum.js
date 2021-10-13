e013e2c261e920709d5970c864d5a8e8c36d6a59
Ryan May <ryan777may@gmail.com>
2021-10-13 19:26:38 -0400

    Created function to check for duplicate paintings

--- src/Components/Museum.js
+++ src/Components/Museum.js
@@ -1,63 +1,107 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
-
+import {useEffect, useState} from "react";
 
 // API KEY: w2HhIYck
 
 const Museum = (props) => {
-  const [ artwork, setArtwork] = useState();
-  const [error, setError] = useState()
-  const color = " " + props.colorChoice; //add a space cause api is retarded af
-  const randomPainting = (artArray) => {
-    const paintings = artArray.artObjects
-    const painting = paintings[Math.floor(Math.random() * paintings.length)];
-    setArtwork(painting);
-  }
+    const artwork = props.artwork;
+    const updateArtwork = props.updateArtwork;
+    const [error,
+        setError] = useState()
+    const color = " " + props.colorChoice; //add a space cause api is retarded af
+    const [artworkPresent,
+        setArtworkPresent] = useState(false);
 
 
+    useEffect(() => {
+      const randomPainting = (artArray) => {
+        const paintings = artArray.artObjects
+        const randomize = () => paintings[Math.floor(Math.random() * paintings.length)];
+        let newPainting = randomize()
+        let repeat = false;
 
-  useEffect( () => {
-    axios({
-        url: 'https://www.rijksmuseum.nl/api/en/collection',
-        method: 'GET',
-        dataResponse: 'json',
-        params: {
-            key: 'w2HhIYck',
-            type: 'painting',
-            'f.normalized32Colors.hex': color,
+    if (artwork.length === 0) { //Skip repeat check if gallery is just being started
+          const galleryArr = artwork;
+          galleryArr.push(newPainting)
+          updateArtwork(galleryArr)
+          setArtworkPresent(true)
+
+    } else {
+
+      let x = 0;
+      while (x < artwork.length) {
+        if(artwork[x].id === newPainting.id){ //Check if existing paintings id's match new painting
+          repeat = true;
+          break
         }
-    }).then(response => { 
-      if(response && response.data.artObjects.length !== 0) {
-        randomPainting(response.data)
-      } else {
-        throw new Error()
+        x++
       }
 
-    }).catch(error => setError('An error occurred, please try a different selection or try again later'))
 
-  }, [color])
-
-
+      if (repeat === false) { //If there are no repeats, push the painting to the gallery
+        const galleryArr = artwork;
+        galleryArr.push(newPainting)
+        updateArtwork(galleryArr)
+        setArtworkPresent(true)
+      }
 
  
+      else if (artwork.length !== artArray.artObjects.length){ //If we havent reached the end of the results and there is a repeat, run this function again for a new painting
+        randomPainting(artArray)
+      }else { //
+        setArtworkPresent(true) //Show gallery and notify user we have reached the end of the results
+        setError('All results have been shown')
+      }
+    }
 
-  return (
-    <div className='artContainer'>
-      {
-        artwork ? 
-        <div className='art'>
-        <img src={artwork.webImage.url} alt={artwork.title} />
-        <h3>{artwork.title}</h3>
+
+    }
+
+        axios({
+            url: 'https://www.rijksmuseum.nl/api/en/collection',
+            method: 'GET',
+            dataResponse: 'json',
+            params: {
+                key: 'w2HhIYck',
+                type: 'painting',
+                'f.normalized32Colors.hex': color
+            }
+        }).then(response => {
+            if (response && response.data.artObjects.length !== 0) {
+                randomPainting(response.data)
+            } else {
+                throw new Error()
+            }
+
+        }).catch(error => setError('An error occurred, please try a different selection or try again later'))
+
+    }, [color, artwork, updateArtwork])
+
+    return (
+      <div className='artContainer'>
+            {artworkPresent
+                ? <div className="art">
+                        {artwork.map((art, index) => {
+                            return (
+                                <div className='art' key={index}>
+                                    <img src={art.webImage.url} alt={art.title}/>
+                                    <h3>{art.title}</h3>
+                                </div>
+                            )
+                        })
+                        }
+                        <button className='clearButton' onClick={() => {props.clearGallery()}}>Clear Gallery</button>
+                    </div>
+                : <p>Getting some artwork, please wait</p>
+}
+
+            {error
+                ? <p>{error}</p>
+                : null
+}
+
         </div>
-        : <p>Getting some artwork, please wait</p>
-      }
-      {
-        error ? <p>{error}</p>: null
-      }
-
-      
-    </div>
-  )
+    )
 }
 
 export default Museum;
