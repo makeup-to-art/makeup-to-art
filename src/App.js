47c2684399d7715a58e43764b50700112b6421a2
Ryan May <ryan777may@gmail.com>
2021-10-14 16:28:29 -0400

    Fixed issues with multiple paintings showing when brand was switched

--- src/App.js
+++ src/App.js
@@ -28,8 +28,10 @@ function App() {
         setUserSelect] = useState('userSelect')
     const [screen,
         setScreen] = useState('AppScreen1');
-    const [ artwork, 
-        setArtwork] = useState([])
+    const [artwork,
+        setArtwork] = useState([]);
+    const [getArtwork,
+        setGetArtwork] = useState(false);
 
     const compareColor = nearestColor.from(colors); //setup nearest color comparison package, checks inputted colour against provided color array
 
@@ -44,6 +46,7 @@ function App() {
                 }
             }).then(response => {
                 if (response.data.length !== 0 && response) {
+                    setSubmit(false);
                     const brandProducts = response.data;
                     const colors = [];
                     brandProducts.forEach(product => {
@@ -53,17 +56,16 @@ function App() {
                     })
 
                     const randomColours = [];
-                    randomizer(colors, randomColours) //Goes through the colors array and performs a forEach on the array and any sub arrays to grab random colours until a total of 7 are achieved.
+                    randomizer(colors, randomColours); //Goes through the colors array and performs a forEach on the array and any sub arrays to grab random colours until a total of 7 are achieved.
                     setProductColors(randomColours);
-                    setSubmit(false);
-                    setArrayStatus(true)
+                    setArrayStatus(true);
                 } else {
-                    throw new Error()
+                    throw new Error();
                 }
 
-
             }).catch(error => {
-                setApiError('An error occured, please try again. If this re occurs, try a different brand or try again at a later time');
+                setApiError('An error occured, please try again. If this re occurs, try a different brand or ' +
+                        'try again at a later time');
             })
 
         }
@@ -71,18 +73,21 @@ function App() {
 
     const handleSubmit = (e, brand) => {
         e.preventDefault();
-        setProductColors([]) //Reset colours so the previous colours don't get used
+        setProductColors([]); //Reset colours so the previous colours don't get used
         setBrandName(brand);
         setSubmit(true);
-        setUserSelect('userSelectMade')
-        setScreen('AppScreen2')
+        setUserSelect('userSelectMade');
+        setScreen('AppScreen2');
+        setGetArtwork(false) //Prevents api call in museum from running before a colour has been chosen
     }
 
     const handleColorChoice = (e) => {
         setColorChoice('');
-        //Timeout is used so that the museum component has time to realize the colour has changed and will remove the last image.
+        // Timeout is used so that the museum component has time to realize the colour
+        // has changed and will remove the last image.
         setTimeout(() => {
             const closestColor = compareColor(e.target.value); //Checks which colour is closest to the one the user selected
+            setGetArtwork(true);
             setColorChoice(closestColor);
         }, 10)
 
@@ -93,7 +98,7 @@ function App() {
     }
 
     const clearGallery = () => {
-        updateArtwork([])
+        updateArtwork([]);
         setColorChoice();
         // setArtworkPresent(false);
     }
@@ -105,7 +110,12 @@ function App() {
                 : <p>{apiError}</p>
 }
             {colorChoice
-                ? <Museum colorChoice={colorChoice} artwork={artwork} updateArtwork={updateArtwork} clearGallery={clearGallery}/>
+                ? <Museum
+                        colorChoice={colorChoice}
+                        artwork={artwork}
+                        updateArtwork={updateArtwork}
+                        clearGallery={clearGallery}
+                        getArtwork={getArtwork}/>
                 : null
 }
             <Footer/>
