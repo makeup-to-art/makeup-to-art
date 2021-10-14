6ea8c1325abe02705548020d9d51494f6bf5b43b
Bree <breehorton25@gmail.com>
2021-10-14 15:01:04 -0400

    css update

--- src/Components/BrandColor.js
+++ src/Components/BrandColor.js
@@ -3,21 +3,21 @@ const BrandColor = (props) => {
 
     
     return(
-        <div className="colourContainer">
-        <h2>Pick a color below & we will find a painting to match your look!</h2>
-        <ul className="colorList">
-            {
-                props.colorArray.map((color, index) => {
+        <section className="colourContainer wrapper">
+            <h2>Pick a color below & we will find a painting to match your look!</h2>
+            <ul className="colorList">
+                {
+                    props.colorArray.map((color, index) => {
 
-                    return(
-                        <li key={index}>
-                                <button className="colorBox" style={{backgroundColor: `${color}`}} onClick={props.handleColorChoice} value={color}></button>
-                        </li>
-                    )
-                })
-            }
-        </ul>
-        </div>
+                        return(
+                            <li key={index}>
+                                    <button className="colorBox" style={{backgroundColor: `${color}`}} onClick={props.handleColorChoice} value={color}></button>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </section>
     )
 }
 
