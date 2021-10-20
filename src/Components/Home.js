0c5bed1fe4d8a88138edff4f5996b61d0fc5ad47
Ryan May <ryan777may@gmail.com>
2021-10-20 18:40:30 -0400

    Cleaned up code and improved readability

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,82 +1,32 @@
-import { useState } from "react";
+import {useState} from "react";
+import brands from "../Utilities/brands";
 
 const Home = (props) => {
 
-    const [selectedBrand, setSelectedBrand] = useState('');
-    const brands = ["almay",
-        "alva",
-        "anna sui",
-        "annabelle",
-        "benefit",
-        "boosh",
-        "burt's bees",
-        "butter london",
-        "cargo cosmetics",
-        "c'est moi",
-        "china glaze",
-        "clinique",
-        "coastal classic creation",
-        "colourpop",
-        "covergirl",
-        "dalish",
-        "deciem",
-        "dior",
-        "dr. hauschka",
-        "e.l.f.",
-        "essie",
-        "fenty",
-        "glossier",
-        "green people",
-        "iman",
-        "L'Oreal",
-        "lotus cosmetics usa",
-        "maia's mineral galaxy",
-        "marcelle",
-        "marienatie",
-        "maybelline",
-        "milani",
-        "mineral fusion",
-        "misa",
-        "mistura",
-        "moov",
-        "nudus",
-        "nyx",
-        "orly",
-        "pacifica",
-        "penny lane organics",
-        "physicians formula",
-        "piggy paint",
-        "pure anada",
-        "rejuva minerals",
-        "revlon",
-        "sally b's skin yummies",
-        "salon perfect",
-        "sante",
-        "sinful colours",
-        "smashbox",
-        "stila",
-        "suncoat",
-        "w3llpeople",
-        "wet n wild",
-        "zorah",
-        "zorah biocosmetiques"];
-    
+    const [selectedBrand,
+        setSelectedBrand] = useState('');
 
 
     return (
-        <form className={props.userSelect} onSubmit={ (e => props.handleSubmit(e, selectedBrand))}>
+        <form
+            className={props.userSelect}
+            onSubmit={(e => props.handleSubmit(e, selectedBrand))}>
             <h1 className="headline">Hue Are You?!</h1>
             <label htmlFor="brandSelect">Pick your Favourite Makeup Brand</label>
 
-            <select name="brandSelect" id="brandSelect" required onChange={(e => setSelectedBrand(e.target.value))} value={selectedBrand}>
+            <select
+                name="brandSelect"
+                id="brandSelect"
+                required
+                onChange={(e => setSelectedBrand(e.target.value))}
+                value={selectedBrand}>
                 <option value="" disabled>Select a makeup brand</option>
-                {
-                  brands.map( (brand, index) => {
+                {brands.map((brand, index) => {
                     return (
-                      <option key={index} value={brand}>{brand}</option>
-                      )
-                    })
-                  }
+                        <option key={index} value={brand}>{brand}</option>
+                    )
+                })
+}
             </select>
             <button type="submit">Select Brand</button>
         </form>
